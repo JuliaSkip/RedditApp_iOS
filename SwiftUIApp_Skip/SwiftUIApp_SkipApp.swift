b19a3f1a4c182fa597712fa535fce321c1f6463c
Julia Skip <skipjulija01@gmail.com>
2025-04-21 12:09:54 +0300

    Initial Commit

---
+++ SwiftUIApp_Skip/SwiftUIApp_SkipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp_SkipApp.swift
+//  SwiftUIApp_Skip
+//
+//  Created by Скіп Юлія Ярославівна on 21.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp_SkipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
